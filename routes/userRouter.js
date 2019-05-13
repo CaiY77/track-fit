d4151f3ada9d523faaa6405ac7031dfea8c9519c
Cai Yang <caih.yang@gmail.com>
2019-05-13 14:40:57 -0400

    create user route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 userRouter.use(bodyParser.json());
 
+// get single user
 userRouter.get('/:id',async(req,res)=>{
     try{
         const users = await User.findByPk(req.params.id);
@@ -15,7 +16,32 @@ userRouter.get('/:id',async(req,res)=>{
     }
 })
 
+// make new user
+userRouter.post('/create', async(req,res)=>{
+    try{
+        const newUser = await User.create(req.body);
+        res.send(newUser)
+    }
+    catch(e){
+        console.log('Something went wrong: ${e}')
+    }
+})
 
+
+// create food entry
+
+userRouter.post('/:id/foodEntry', async (req, res) => {
+  try {
+    const newFoodEntry = await Food.create(req.body)
+    res.send(newFoodEntry)
+  } catch (e) {
+    console.log(e)
+  }
+
+})
+
+
+//delete food
 
 module.exports = {
   userRouter
