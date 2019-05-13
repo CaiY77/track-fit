25b4c59317b0474a654adf5a6898cb5fc0f266aa
Cai Yang <caih.yang@gmail.com>
2019-05-13 12:24:19 -0400

    created 3 datatables

--- models.js
+++ models.js
@@ -9,8 +9,30 @@ const db = new Sequelize({
     }
 })
 
+const User = db.define('data',{
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.INTEGER
+})
 
+const Goal = db.define('goal',{
+  execiseGoal: Sequelize.INTEGER,
+  foodGoal: Sequelize.INTEGER
+})
+
+const FoodEntries = db.define('entries',{
+  sportsKind = Sequelize.STRING,
+  foodKind = Sequelize.STRING
+})
+
+const ExerciseEntries = db.define('entries',{
+  sportsKind = Sequelize.STRING,
+  foodKind = Sequelize.STRING
+})
 
 module.export = {
   db,
+  User,
+  Goal,
+  Entries
 }
