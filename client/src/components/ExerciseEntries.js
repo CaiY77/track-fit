15fc8bd295aa98326dfeca59d63859ee958f02dc
Mingjing Tang <tmjcsh@gmail.com>
2019-05-14 14:41:50 -0400

    exercise entries works

--- client/src/components/ExerciseEntries.js
+++ client/src/components/ExerciseEntries.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
-
+import {fetchExercise} from '../service/track-fit'
+import {Card} from 'semantic-ui-react'
+const moment = require('moment');
 
 class ExerciseEntries extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      allExercise:[],
+      getExercise:false,
+    }
+  }
+
+  componentDidMount(){
+      this.getAll();
+  }
+
+  showExercise=()=>{
+    const {allExercise} = this.state;
+    const myCards = allExercise.map(exercise =>{
+      const dateString = exercise.date;
+      // const dateAnswer = new Date(dateString)
+      const momentDate = moment(dateString)
+      return (
+        <Card>
+          <h1>{exercise.exercise}</h1>
+          <h1>{exercise.calBurned}</h1>
+          <h1>{momentDate.format("YYYY-MM-DD")}</h1>
+        </Card>
+      )
+    })
+    return myCards;
+  }
+
+  getAll = async()=>{
+    const allExercise = await fetchExercise(this.props.user)
+    this.setState({
+      allExercise: allExercise,
+      getExercise: true
+    });
+  }
 
   render() {
     return (
-      <div>ExerciseEntries</div>
+      (this.state.getExercise)
+      ?this.showExercise()
+      :<h1>No Exercise Entries Exist</h1>
     );
   }
-
 }
 
 export default ExerciseEntries;
