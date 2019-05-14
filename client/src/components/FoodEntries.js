c68640c9b29459d75bed0988bd4f9af6a3a8d491
Cai Yang <caih.yang@gmail.com>
2019-05-14 11:52:56 -0400

    nav bar

--- client/src/components/FoodEntries.js
+++ client/src/components/FoodEntries.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {fetchFood} from '../service/track-fit'
+import {Card} from 'semantic-ui-react'
 
   class FoodEntries extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        allFood: []
+        allFood: [],
+        getFood: false
       };
     }
 
@@ -15,18 +17,35 @@ import {fetchFood} from '../service/track-fit'
 
     showEntries = () =>{
       const {allFood} = this.state;
+      const myCards = allFood.map(entry =>{
+        return (<Card>
+          <h1>{entry.food}</h1>
+          <h1>{entry.calGained}</h1>
+          <h1>{entry.date.substr(0,10)}</h1>
+        </Card>)
+      })
+
+      return myCards;
     }
 
     getAll = async() => {
       const allFoods = await fetchFood(this.props.user)
       this.setState({
-        allFood: allFoods
+        allFood: allFoods,
+        getFood: true
       });
     }
 
     render() {
       return (
-        <div></div>
+        <div>
+
+          {
+            (this.state.getFood)
+              ? this.showEntries()
+              : <h1>No Entries Exist</h1>
+          }
+        </div>
       );
     }
 
