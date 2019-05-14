0dcfa8fcbf36b008c9fbbd64a05b6aa07c179983
Cai Yang <caih.yang@gmail.com>
2019-05-13 21:28:08 -0400

    create components

---
+++ client/src/components/ExerciseEntries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExerciseEntries extends Component {
+
+  render() {
+    return (
+      <div>ExerciseEntries</div>
+    );
+  }
+
+}
+
+export default ExerciseEntries;
--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Route,Link} from 'react-router-dom';
+import Profile from './Profile'
+import FoodEntries from './FoodEntries'
+import ExerciseEntries from './ExerciseEntries'
 
 class ProfilePage extends Component {
 
@@ -8,8 +11,8 @@ class ProfilePage extends Component {
       <div>
 
         <Link to="/">Profile</Link>
-        <Link to="/food-entries">Profile</Link>
-        <Link to="/exercise-entries">Profile</Link>
+        <Link to="/food-entries">Food Entries</Link>
+        <Link to="/exercise-entries">Exercise Entries</Link>
 
         <Route exact path='/'
           render={()=> <Profile
