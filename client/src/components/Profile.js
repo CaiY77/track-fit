05f1323e1884c25bd5e53369ea1aa7617df7c6d3
Mingjing Tang <tmjcsh@gmail.com>
2019-05-15 14:49:30 -0400

    profile basic structure

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {fetchUser} from '../service/track-fit.js';
+import { Image, Segment, Button } from 'semantic-ui-react'
 
 class Profile extends Component {
   constructor(props) {
@@ -20,16 +21,32 @@ class Profile extends Component {
       user: myUser
     });
   } 
-  
+
+
   render() {
     // console.log(this.props.user);
 
     return (
       <div>
         <h1>Profile</h1>
-        <p>
-          {this.state.user.name}
-        </p>
+        <img style={{width: "200px", height: "auto" }}
+            src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+            class="ui medium circular image"
+        />
+        <div class="ui raised segments">
+            <div class="ui segment">
+                {this.state.user.name}
+                {/* <button class="ui negative button">Update Name</button> */}
+            </div>
+            <div class="ui segment">
+                {this.state.user.email}
+                {/* <button class="ui negative button">Update Email</button> */}
+            </div>
+            <div class="ui segment">
+                {this.state.user.password}
+                {/* <button class="ui negative button">Update Password</button> */}
+            </div>
+        </div>
       </div> 
     );
   }
