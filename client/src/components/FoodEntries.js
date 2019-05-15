8a5027cb5f8dc7324055d45395294d13ec6a011b
Cai Yang <caih.yang@gmail.com>
2019-05-15 04:27:42 -0400

    background and shade

--- client/src/components/FoodEntries.js
+++ client/src/components/FoodEntries.js
@@ -155,98 +155,97 @@ const CalorieOptions = [
       return (
 
         <div className="display-contain">
-          <div className="display-left">
-            <div className="button-modal">
-              <Modal trigger={<Button inverted color="green" size="huge" className ="add-button">Add Food Entry</Button>}>
-                <h1 className="modal-style">Add To Your Food Log</h1>
-                <Segment>
-                  <Grid columns={2} relaxed='very'>
-                    <Grid.Column>
+          <div className="shade">
+            <div className="display-left">
+              <div className="button-modal">
+                <Modal trigger={<Button inverted color="green" size="huge" className ="add-button">Add Food Entry</Button>}>
+                  <h1 className="modal-style">Add To Your Food Log</h1>
+                  <Segment>
+                    <Grid columns={2} relaxed='very'>
+                      <Grid.Column>
 
-                      <Form onSubmit={()=>this.addNewFood()}>
-                        <Form.Field required>
-                          <label>Food Consumed</label>
-                          <input onChange={this.handleChanges} name="food" placeholder='Today, I had some ...' />
-                        </Form.Field>
-                        <Form.Field required>
-                          <label>Calories</label>
-                          <input onChange={this.handleChanges} name="calGained" placeholder='Enter Calories' />
-                        </Form.Field>
-                        <Form.Field required>
-                          <label>Date</label>
-                          <input onChange={this.handleChanges} type="datetime-local" name="date" />
-                        </Form.Field>
-                        <Button inverted color="green" type='submit'>Submit</Button>
-                      </Form>
+                        <Form onSubmit={()=>this.addNewFood()}>
+                          <Form.Field required>
+                            <label>Food Consumed</label>
+                            <input onChange={this.handleChanges} name="food" placeholder='Today, I had some ...' />
+                          </Form.Field>
+                          <Form.Field required>
+                            <label>Calories</label>
+                            <input onChange={this.handleChanges} name="calGained" placeholder='Enter Calories' />
+                          </Form.Field>
+                          <Form.Field required>
+                            <label>Date</label>
+                            <input onChange={this.handleChanges} type="datetime-local" name="date" />
+                          </Form.Field>
+                          <Button inverted color="green" type='submit'>Submit</Button>
+                        </Form>
 
-                    </Grid.Column>
-                    <Grid.Column>
-                      <Form onSubmit={()=>this.addNewFood()}>
-                        <Form.Field required>
-                          <label>Food Consumed</label>
-                          <Form.Select onChange={(e, {value}) => this.handleFood(value)} options={FoodOptions} name="food" placeholder='Select Food' />
-                        </Form.Field>
-                        <Form.Field required>
-                          <label>Calories</label>
-                          <Form.Select onChange={(e, {value}) => this.handleCal(value)} options={CalorieOptions} name="calGained" placeholder='Select Calories' />
-                        </Form.Field>
-                        <Form.Field required>
-                          <label>Date</label>
-                          <input onChange={this.handleChanges} type="datetime-local" name="date" />
-                        </Form.Field>
-                        <Button inverted color="green" type='submit'>Submit</Button>
-                      </Form>
-                    </Grid.Column>
-                  </Grid>
-                  <Divider vertical>OR</Divider>
-                </Segment>
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Form onSubmit={()=>this.addNewFood()}>
+                          <Form.Field required>
+                            <label>Food Consumed</label>
+                            <Form.Select onChange={(e, {value}) => this.handleFood(value)} options={FoodOptions} name="food" placeholder='Select Food' />
+                          </Form.Field>
+                          <Form.Field required>
+                            <label>Calories</label>
+                            <Form.Select onChange={(e, {value}) => this.handleCal(value)} options={CalorieOptions} name="calGained" placeholder='Select Calories' />
+                          </Form.Field>
+                          <Form.Field required>
+                            <label>Date</label>
+                            <input onChange={this.handleChanges} type="datetime-local" name="date" />
+                          </Form.Field>
+                          <Button inverted color="green" type='submit'>Submit</Button>
+                        </Form>
+                      </Grid.Column>
+                    </Grid>
+                    <Divider vertical>OR</Divider>
+                  </Segment>
 
-              </Modal>
+                </Modal>
+
+              </div>
+              <Card.Group itemsPerRow={2} className="card-group">
+                {
+                  (this.state.allFood.length !== 0)
+                    ? this.showEntries()
+                    : <h1>No Entries Exist</h1>
+                }
+              </Card.Group>
+            </div>
+            <div className="display-right">
+              <CircularProgressbar
+                className="progress"
+                value={this.totalCal()/maxCal * 100}
+                text={
+                  (this.totalCal()/maxCal * 100 > 100)
+                    ? `Yikes!`
+                    :`~ ${Math.floor(this.totalCal()/maxCal * 100)} %`
+                }
+              />
+              <Divider section className="divide"/>
+              <Statistic className ="stats">
+                <Statistic.Label>Consumed</Statistic.Label>
+                <Statistic.Value>{this.totalCal()}</Statistic.Value>
+                <Statistic.Label>Calories So Far</Statistic.Label>
+              </Statistic>
+              <Divider section className="divide"/>
+              <Statistic className ="stats">
+                <Statistic.Label>You Set a </Statistic.Label>
+                <Statistic.Value>{maxCal}</Statistic.Value>
+                <Statistic.Label>Calorie Limit</Statistic.Label>
+              </Statistic>
+              <Divider section className="divide"/>
+              <Statistic className ="stats">
+                <Statistic.Label>You Have a Total of</Statistic.Label>
+                <Statistic.Value>{this.state.allFood.length}</Statistic.Value>
+                <Statistic.Label>ENTRIES</Statistic.Label>
+              </Statistic>
+
 
             </div>
-            <Card.Group itemsPerRow={2} className="card-group">
-              {
-                (this.state.allFood.length !== 0)
-                  ? this.showEntries()
-                  : <h1>No Entries Exist</h1>
-              }
-            </Card.Group>
           </div>
-          <div className="display-right">
-            <CircularProgressbar
-              className="progress"
-              value={this.totalCal()/maxCal * 100}
-              text={
-                (this.totalCal()/maxCal * 100 > 100)
-                  ? `Yikes!`
-                  :`~ ${Math.floor(this.totalCal()/maxCal * 100)} %`
-              }
-            />
-            <Divider section className="divide"/>
-            <Statistic className ="stats">
-              <Statistic.Label>Consumed</Statistic.Label>
-              <Statistic.Value>{this.totalCal()}</Statistic.Value>
-              <Statistic.Label>Calories So Far</Statistic.Label>
-            </Statistic>
-            <Divider section className="divide"/>
-            <Statistic className ="stats">
-              <Statistic.Label>You Set a </Statistic.Label>
-              <Statistic.Value>{maxCal}</Statistic.Value>
-              <Statistic.Label>Calorie Limit</Statistic.Label>
-            </Statistic>
-            <Divider section className="divide"/>
-            <Statistic className ="stats">
-              <Statistic.Label>You Have a Total of</Statistic.Label>
-              <Statistic.Value>{this.state.allFood.length}</Statistic.Value>
-              <Statistic.Label>ENTRIES</Statistic.Label>
-            </Statistic>
-
-
-          </div>
-
-        </div>
-
-      );
+        </div>);
     }
 
   }
