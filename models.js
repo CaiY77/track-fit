72ac33d84ac9bf5b4aa95fb5d552f58bece3abe3
Cai Yang <caih.yang@gmail.com>
2019-05-17 15:53:30 -0400

    deploy

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize({
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/trackfit_db',{
     database: 'trackfit_db',
     dialect: 'postgres',
     define:{
